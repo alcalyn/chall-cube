13fa17c7b68a3eb8c59f8aaed99cecb7d5541053
Julien Maulny <doubjulien@hotmail.fr>
2017-07-07 16:38:35 +0200

    rename cube to chall_cube

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cube',
+    name='chall-cube',
     version='1.0',
-    description='Base API for RaspberryPi cube.',
-    url='http://github.com/alcalyn/cube',
+    description='Base API for RaspberryPi challenges cube.',
+    url='http://github.com/alcalyn/chall-cube',
     author='Alcalyn',
     author_email='doubjulien@hotmail.fr',
     license='AGPL-3.0',
