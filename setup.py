170cbf347757309e5e049b39b7d8b86dd6c32506
Julien Maulny <doubjulien@hotmail.fr>
2017-09-04 11:58:13 +0200

    release 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chall-cube',
-    version='1.0.4',
+    version='1.0.5',
     description='Base API for RaspberryPi challenges cube.',
     url='http://github.com/alcalyn/chall-cube',
     author='Alcalyn',
